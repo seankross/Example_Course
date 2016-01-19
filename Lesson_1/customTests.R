efffc0935035bc87c32ba4812395af91b9866564
seankross <smk240@gmail.com>
2016-01-19 15:38:37 -0500

    first commit

---
+++ Lesson_1/customTests.R
@@ -0,0 +1,12 @@
+# Put custom tests in this file.
+
+test_func1 <- function() {
+  try({
+    func <- get('boring_function', globalenv())
+    t1 <- identical(func(9), 9)
+    t2 <- identical(func(4), 4)
+    t3 <- identical(func(0), 0)
+    ok <- all(t1, t2, t3)
+  }, silent = TRUE)
+  exists('ok') && isTRUE(ok)
+}
